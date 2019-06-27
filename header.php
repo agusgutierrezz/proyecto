a7e97ccc378d4fdc670200f47c382f4762f5f5df
agus <agusgutierrezperez@gmail.com>
2019-06-27 20:16:58 -0300

    carrito css

--- header.php
+++ header.php
@@ -14,16 +14,17 @@
    <li class="nav-item active">
      <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
    </li>
+   <?php if(estaLogueado()):?>
+     <form  action="cerrar.php" method="post">
+      <button id="botones" class="btn btn-outline-success my-2 my-sm-0" type="submit" name="sesion">Cerrar Sesion</button>
+    </form>
+     <a href="perfil.php"> <button id="botones" class="btn btn-outline-success my-2 my-sm-0" >Mi Perfil</button></a>
+  <?php endif ?>
 </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
-      <?php if(estaLogueado()):?>
-        <form  action="cerrar.php" method="post">
-         <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="sesion">Cerrar Sesion</button>
-       </form>
-        <a href="perfil.php"> <button class="btn btn-outline-success my-2 my-sm-0" >Mi Perfil</button></a>
-     <?php endif ?>
+
     </div>
   </nav>
