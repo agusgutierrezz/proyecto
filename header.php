169d946970b0f67c5e384632195a81611ea8596b
agus <agusgutierrezperez@gmail.com>
2019-06-26 22:23:30 -0300

    navbar

--- header.php
+++ header.php
@@ -4,10 +4,8 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="#">
-    <i class="fas fa-star-of-life" aria-hidden="true"></i>
-    <span> Feriate.com</span>
-    <!---  <img src="./images/logonegro1.png" class="fas fa-heart" aria-hidden="true">--->
-    </a>
+        Feriate.com  <i class="fas fa-store"></i>
+      </a>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
     <?php if(!estaLogueado()):?>
@@ -17,15 +15,6 @@
      <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
    </li>
 </ul>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0" style="visibility:hidden;">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
